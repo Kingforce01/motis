ee2627f78b32844e6d142f83f593381ca29fba42
Alexander Sinkovic <alexander.sinkovic@gmail.de>
2022-03-16 14:53:38 +0100

    Initial commit

---
+++ _next/static/mwja9zOZD3m4p2RZutno7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-4d2f0a9f494a9dad.js","static/chunks/375-76d793711aff1c81.js","static/chunks/pages/index-3a72f739a2f5ea92.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
